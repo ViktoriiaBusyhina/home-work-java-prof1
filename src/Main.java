5d42a12d43477dcbbc3d644b75474544a8c69d88
viktoria <viiikiii.g@gmail.com>
2022-12-18 20:48:31 +0100

    commit massage

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        Person person = new Person("Viktor", 54, 165,64);
+
+
+        person.infoperson();
+        person.gotowork();
+
+        Person secondPerson = new Person ("Tolya", 13, 120, 40);
+
+        secondPerson.infoperson();
+        secondPerson.gotowork();
+
+
+
+    }
+}
